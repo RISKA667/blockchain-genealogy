407ad7a04b86e0a45a9bc5e0c114cc228579423d
Octavius <100957976+RISKA667@users.noreply.github.com>
2025-09-08 16:14:14 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-
-// Import pour les toast notifications
 import 'react-hot-toast'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Root element not found')
+}
+
+const root = createRoot(rootElement)
+
+root.render(
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>
 )
